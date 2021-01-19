0234734648f738109147a94da7d340417a768919
Hyunjin Choi <chjin0725@gmail.com>
2021-01-19 15:32:54 +0900

    Update my code

--- text_processing.py
+++ text_processing.py
@@ -8,7 +8,7 @@ NLP에서 흔히하는 전처리는 소문자 변환, 앞뒤 필요없는 띄어
 """
 
 
-def normalize(input_string):
+def normalize(input_string: str) -> str:
     """
      인풋으로 받는 스트링에서 정규화된 스트링을 반환함
      아래의 요건들을 충족시켜야함
@@ -33,11 +33,21 @@ def normalize(input_string):
              >>> tp.normalize(input_string2)
              'extra space'
     """
-    normalized_string = None
+    temp = input_string.lower().strip()
+    splited = temp.split()
+
+    temp_chars = [0 for _ in range(2*len(splited) - 1)]
+    temp_chars[0] = splited[0]
+    for i in range(1, len(splited)):
+        temp_chars[i*2 - 1] = ' '
+        temp_chars[i*2] = splited[i]
+    
+
+    normalized_string = ''.join(temp_chars)
     return normalized_string
 
 
-def no_vowels(input_string):
+def no_vowels(input_string: str) -> str:
     """
     인풋으로 받는 스트링에서 모든 모음 (a, e, i, o, u)를 제거시킨 스트링을 반환함
 
@@ -58,5 +68,12 @@ def no_vowels(input_string):
             >>> tp.normalize(input_string2)
             ''W lv Pythn!'
     """
-    no_vowel_string = None
+    temp_chars = []
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+
+    for c in input_string:
+        if c not in vowels:
+            temp_chars.append(c)
+    no_vowel_string = ''.join(temp_chars)
+
     return no_vowel_string
